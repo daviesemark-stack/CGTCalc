54533ef13a84a1c71c15961b4c5f2fc4607ef08c
Mark Davies <mdavies@chandler.com.au>
2026-05-20 14:02:09 +1000

    Add GitHub Pages deployment workflow

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = (env, argv) => ({
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
+    publicPath: 'auto',
     clean: true,
   },
   resolve: {
